a5c23d99987dc04e629df8f8d0b80fae2e9625b7
b00llean <kyudayev@gmail.com>
2017-05-26 12:15:53 -0400

    Create Priest-Holy.lua

---
+++ Priest-Holy.lua
@@ -0,0 +1,117 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "HW Sanctify",
+        [2] = "HW Sanctify - CD",
+        [3] = "HW Serenity",
+        [4] = "HW Serenity - CD",
+        [5] = "Halo",
+        [6] = "Light of Tuure",
+        [7] = "Divine Hymn",
+        [8] = "Divine Hymn - CD",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["disjunctive"] = "all",
+    ["border"] = false,
+    ["untrigger"] = {
+    },
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["activeTriggerMode"] = -10,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["xOffset"] = -3.0000610351562,
+    ["borderOffset"] = 5,
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+    ["id"] = "Priest - Holy",
+    ["anchorPoint"] = "CENTER",
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["trigger"] = {
+        ["type"] = "aura",
+        ["spellIds"] = {
+        },
+        ["unit"] = "player",
+        ["debuffType"] = "HELPFUL",
+        ["names"] = {
+        },
+    },
+    ["borderInset"] = 11,
+    ["numTriggers"] = 1,
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["borderEdge"] = "None",
+    ["yOffset"] = -101.99984741211,
+    ["load"] = {
+        ["talent2"] = {
+            ["multi"] = {
+            },
+        },
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["difficulty"] = {
+            ["multi"] = {
+            },
+        },
+        ["race"] = {
+            ["multi"] = {
+            },
+        },
+        ["use_class"] = "true",
+        ["pvptalent"] = {
+            ["multi"] = {
+            },
+        },
+        ["role"] = {
+            ["multi"] = {
+            },
+        },
+        ["faction"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "PRIEST",
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["expanded"] = false,
+}
