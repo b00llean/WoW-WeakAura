5140e7fc0cfbfeb6c1bf6d8d1546b8a2198983f8
b00llean <kyudayev@gmail.com>
2017-05-26 12:16:47 -0400

    Create Shaman-Elemental.lua

---
+++ Shaman-Elemental.lua
@@ -0,0 +1,143 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Maelstrom Bar Colourchanging",
+        [2] = "Maelstrom Predict Bar Colourchanging",
+        [3] = "Mana Bar",
+        [4] = "FS - active",
+        [5] = "FS - shouldCast",
+        [6] = "FS- mustCast",
+        [7] = "LvB - cooldown",
+        [8] = "LvB - Surge",
+        [9] = "LvB - ready",
+        [10] = "Earthshock - Need to cast",
+        [11] = "Power of the Maelstrom",
+        [12] = "Fire Elemental - Cooldown",
+        [13] = "Fire Elemental - Ready",
+        [14] = "Ascendance - Cooldown",
+        [15] = "Ascendance - Active",
+        [16] = "Ascendance - Ready",
+        [17] = "Stormkeeper - Ready",
+        [18] = "Stormkeeper - Charges",
+        [19] = "Stormkeeper - Cooldown",
+        [20] = "Liquid Magma Totem - Cooldown",
+        [21] = "Liquid Magma Totem - Active",
+        [22] = "Liquid Magma Totem - ShoudCast",
+        [23] = "Totem Mastery - Cooldown",
+        [24] = "Totem Mastery - ShouldCast",
+        [25] = "Icefury - Ready",
+        [26] = "Icefury - Charges",
+        [27] = "Icefury - Cooldown",
+        [28] = "Elemental Blast - Ready",
+        [29] = "Elemental Blast - Active",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["disjunctive"] = "all",
+    ["border"] = false,
+    ["yOffset"] = -108.60922241211,
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["activeTriggerMode"] = -10,
+    ["expanded"] = false,
+    ["untrigger"] = {
+    },
+    ["xOffset"] = -4.8982543945312,
+    ["borderOffset"] = 5,
+    ["id"] = "Shaman - Elemental (bullean)",
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["additional_triggers"] = {
+    },
+    ["borderEdge"] = "None",
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["animation"] = {
+        ["start"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["main"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["finish"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+    },
+    ["borderInset"] = 11,
+    ["numTriggers"] = 1,
+    ["trigger"] = {
+        ["subeventPrefix"] = "SPELL",
+        ["type"] = "aura",
+        ["spellIds"] = {
+        },
+        ["subeventSuffix"] = "_CAST_START",
+        ["debuffType"] = "HELPFUL",
+        ["names"] = {
+        },
+        ["event"] = "Health",
+        ["unit"] = "player",
+    },
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["anchorPoint"] = "CENTER",
+    ["load"] = {
+        ["talent2"] = {
+            ["multi"] = {
+            },
+        },
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "SHAMAN",
+        },
+        ["difficulty"] = {
+            ["multi"] = {
+            },
+        },
+        ["race"] = {
+            ["multi"] = {
+            },
+        },
+        ["use_class"] = "true",
+        ["faction"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["role"] = {
+            ["multi"] = {
+            },
+        },
+        ["pvptalent"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+}
