965997a95dc74840cb61abc2811dc80541f72f21
b00llean <kyudayev@gmail.com>
2017-05-26 12:21:24 -0400

    Create Shaman-Restoration.lua

---
+++ Shaman-Restoration.lua
@@ -0,0 +1,127 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Healing Stream Totem - Active",
+        [2] = "Healing Stream Totem - Ready",
+        [3] = "Healing Tide Totem - Cooldown",
+        [4] = "Healing Tide Totem - Ready",
+        [5] = "Healing Tide Totem - Active",
+        [6] = "Riptide - Ready",
+        [7] = "Spirit Link Totem - Ready",
+        [8] = "Spirit Link Totem - Active",
+        [9] = "Spirit Link Totem - Cooldown",
+        [10] = "Earthen Shield Totem - Ready",
+        [11] = "Earthen Shield Totem - Active",
+        [12] = "Earthen Shield Totem - Cooldown",
+        [13] = "Ancestral Protection Totem - Ready",
+        [14] = "Ancestral Protection Totem - Active",
+        [15] = "Ancestral Protection Totem - Cooldown",
+        [16] = "Healing Rain - Ready",
+        [17] = "Tidal Waves",
+        [18] = "Gift of the Queen",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["disjunctive"] = "all",
+    ["border"] = false,
+    ["untrigger"] = {
+    },
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["activeTriggerMode"] = -10,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
+    ["expanded"] = true,
+    ["borderOffset"] = 5,
+    ["xOffset"] = -118.69024658203,
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["id"] = "Shaman - Restoration (bullean)",
+    ["borderEdge"] = "None",
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["borderInset"] = 11,
+    ["numTriggers"] = 1,
+    ["yOffset"] = -178.29983520508,
+    ["trigger"] = {
+        ["type"] = "aura",
+        ["spellIds"] = {
+        },
+        ["names"] = {
+        },
+        ["debuffType"] = "HELPFUL",
+        ["unit"] = "player",
+    },
+    ["animation"] = {
+        ["start"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["main"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+        ["finish"] = {
+            ["duration_type"] = "seconds",
+            ["type"] = "none",
+        },
+    },
+    ["load"] = {
+        ["talent2"] = {
+            ["multi"] = {
+            },
+        },
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["use_class"] = "true",
+        ["race"] = {
+            ["multi"] = {
+            },
+        },
+        ["difficulty"] = {
+            ["multi"] = {
+            },
+        },
+        ["pvptalent"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "SHAMAN",
+        },
+        ["faction"] = {
+            ["multi"] = {
+            },
+        },
+        ["role"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["anchorPoint"] = "CENTER",
+}
